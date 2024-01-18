0b34adcde8f63dc0f1cdbbe012a221c69ec684f0
Kayle54187 <habichristi@gmail.com>
2024-01-18 11:24:23 +0200

    feat: Initializing Project

---
+++ src/main/java/com/personal/photos/PhotosApplication.java
@@ -0,0 +1,13 @@
+package com.personal.photos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotosApplication.class, args);
+	}
+
+}
